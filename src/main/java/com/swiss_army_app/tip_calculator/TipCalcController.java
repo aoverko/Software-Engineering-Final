f771b9852cd8950c1204f4f05bac647fe0f4e56a
jewellobott <jo459@cairn.edu>
2025-04-22 18:10:43 -0400

    Update TipCalcController.java
    added code!

--- src/main/java/com/swiss_army_app/tip_calculator/TipCalcController.java
+++ src/main/java/com/swiss_army_app/tip_calculator/TipCalcController.java
@@ -1,4 +1,17 @@
 package com.swiss_army_app.tip_calculator;
 
-public class TipCalcController {
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class TipCalcController{
+
+    @FXML
+    private TextField totalBillField;
+
+    @FXML
+    private TextField totalTipField;
+
+    @FXML
+    private TextField splitBillField;
+
 }
