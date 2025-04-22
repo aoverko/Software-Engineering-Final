f422eab7fbdc686e63e813c09060110d3d3f1d07
Ariana <136840493+aoverko@users.noreply.github.com>
2025-04-22 18:35:21 -0400

    added files for sidebar

---
+++ src/main/java/com/swiss_army_app/SidebarController.java
@@ -0,0 +1,12 @@
+package com.swiss_army_app;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class SidebarController {
+    @FXML
+    private ImageView dashIcon;
+
+    @FXML
+
+}
