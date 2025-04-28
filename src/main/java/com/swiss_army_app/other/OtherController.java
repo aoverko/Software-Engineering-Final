2542bd7d66ac15cd3612ac52ec480fa4f35e6420
Ariana <136840493+aoverko@users.noreply.github.com>
2025-04-28 18:13:33 -0400

    added missing imports

--- src/main/java/com/swiss_army_app/other/OtherController.java
+++ src/main/java/com/swiss_army_app/other/OtherController.java
@@ -1,6 +1,7 @@
 package com.swiss_army_app.other;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class OtherController {
--- src/main/java/com/swiss_army_app/tip_calculator/TipCalcController.java
+++ src/main/java/com/swiss_army_app/tip_calculator/TipCalcController.java
@@ -1,6 +1,7 @@
 package com.swiss_army_app.tip_calculator;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class TipCalcController{
