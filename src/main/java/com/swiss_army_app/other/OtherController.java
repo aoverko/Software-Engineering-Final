e1a7bff97b0a97a7a30b050c82f8d9bd621ff957
jewellobott <jo459@cairn.edu>
2025-04-28 17:00:03 -0400

    Update OtherController.java
    added enter and reset buttons

--- src/main/java/com/swiss_army_app/other/OtherController.java
+++ src/main/java/com/swiss_army_app/other/OtherController.java
@@ -7,5 +7,16 @@ public class OtherController {
 
     @FXML
     private TextField userGuessField;
+     @FXML
+    private Button enterButton;
+
+    @FXML
+    private void handleEnterButtonClick(){System.out.println("Enter button clicked!");}
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private void handleResetButtonClick(){System.out.println("Reset button clicked!");}
 }
 
