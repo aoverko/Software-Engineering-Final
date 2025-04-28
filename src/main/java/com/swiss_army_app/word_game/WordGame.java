a42f33c12d55898013faaf9789e39436724c6f46
TalaGaribay <132804253+TalaGaribay@users.noreply.github.com>
2025-04-28 16:30:01 -0400

    Cat game class and scene work

--- src/main/java/com/swiss_army_app/word_game/WordGame.java
+++ src/main/java/com/swiss_army_app/word_game/WordGame.java
@@ -1,4 +1,25 @@
 package com.swiss_army_app.word_game;
 
 public class WordGame {
+    String guessedLetter;
+    String correctWord;
+    boolean isVisible;
+    int NumOfBody;
+
+    public WordGame() {
+        this.guessedLetter = "";
+        this.correctWord = "";
+        this.isVisible = true;
+        this.NumOfBody = 0;
+
+
+    }
+    public void wrongGuess(){
+        this.NumOfBody += 1;
+    }
+
+    public void setCorrectWord(String correctWord) {
+        this.correctWord = correctWord;
+    }
+
 }
