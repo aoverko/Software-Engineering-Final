0bd1edbcea354ff6c4cfa751bcff81667e5b44d1
jewellobott <jo459@cairn.edu>
2025-04-30 15:30:51 -0400

    Update Other.java
    final version of Other.java

--- src/main/java/com/swiss_army_app/other/Other.java
+++ src/main/java/com/swiss_army_app/other/Other.java
@@ -1,4 +1,4 @@
-// package com.swiss_army_app.other;
+package com.swiss_army_app.other;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,17 +6,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-//public class Other extends Application{
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(Other.class.getResource("other.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//    public static void main(String[] args) {
-//        launch();
-//    }
-//
-//}
+public class Other extends Application{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Other.class.getResource("other.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
+        stage.setTitle("Guess The Number Game");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
+
 
