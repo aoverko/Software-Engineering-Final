1f6407fca4206fb7fde61f8755353c1d6043b349
jewellobott <jo459@cairn.edu>
2025-04-30 15:31:59 -0400

    Update OtherController.java
    final version of OtherController.java

--- src/main/java/com/swiss_army_app/other/OtherController.java
+++ src/main/java/com/swiss_army_app/other/OtherController.java
@@ -1,23 +1,63 @@
 package com.swiss_army_app.other;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+
 public class OtherController {
+
+    private int randomNumber; // <-- class-level variable
 
     @FXML
     private TextField userGuessField;
-     @FXML
-    private Button enterButton;
+
+    @FXML TextField resultsMessageField;
 
     @FXML
-    private void handleEnterButtonClick(){System.out.println("Enter button clicked!");}
+    private Button enterButton;
 
     @FXML
     private Button resetButton;
 
+    private void startNewGame() {
+        randomNumber = (int) (Math.random() * 100) + 1;
+    }
+
+
     @FXML
-    private void handleResetButtonClick(){System.out.println("Reset button clicked!");}
-}
+    private void handleEnterButtonClick(ActionEvent event){
+        System.out.println("Button clicked!"); // Check if the button click is registered
+
+        try {
+            String userGuessText = userGuessField.getText();
+
+            int userGuess = Integer.parseInt(userGuessText);
+            
+            String resultsMessage = "";
+            
+            if (userGuess == randomNumber){
+                resultsMessage = "Correct! You guessed the number.";
+            } else if (userGuess > randomNumber){
+                resultsMessage = "Guess Lower :(";
+            } else {
+                resultsMessage = "Guess Higher :(";
+            }
+
+            resultsMessageField.setText(resultsMessage);
+            userGuessField.clear();
+
+        } catch (NumberFormatException e) {
+            System.out.println("Please use a number");
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    private void handleResetButtonClick(ActionEvent event) {
+        startNewGame();
+        resultsMessageField.setText("Game reset :)");
+    }
+    }
 
